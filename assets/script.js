52aa5c89b183679c7af197e9a6f3ac3f51f67e2c
Steph Heins <stephanie_heins@yahoo.com>
2021-10-16 18:16:58 -0700

    Added color changing js, and a LOT OF TROUBLESHOOTING I think i finally got it working though...

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,30 @@ $("#currentdate").text(today.format("MMM Do, YYYY"));
 
 
 // Change color of blocks depending on time
+function hourTracker() {
+    var currentTime = moment().hour();
 
+    $(".timeblock").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentTime)
+
+        if (blockHour < currentTime) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker();
 
 // Load data from LocalStorage 
